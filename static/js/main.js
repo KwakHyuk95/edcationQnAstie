314d2d10cbee5745dcd9b24eb5c97b6c7ac5237c
KwakHyuk95 <rhkrgur95@gmail.com>
2021-10-07 17:32:46 +0900

    2021.10.07 곽혁 마무리

---
+++ static/js/main.js
@@ -0,0 +1,67 @@
+document.addEventListener('click', selectimg);
+
+function selectimg(e){
+    e.preventDefault();
+    const elem = e.target;
+    const elemId = elem.getAttribute('id');
+    const koi = document.getElementById('koi');
+    const mai = document.getElementById('mai');
+    const eni = document.getElementById('eni');
+    const hii = document.getElementById('hii');
+    const soi = document.getElementById('soi');
+    const sci = document.getElementById('sci');
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    const img4 = document.getElementById('img4');
+    const img5 = document.getElementById('img5');
+    const img6 = document.getElementById('img6');
+    if(elemId == 'img1'){
+        koi.classList.remove('visible');
+        mai.classList.remove('visible');
+        eni.classList.remove('visible');
+        hii.classList.remove('visible');
+        soi.classList.remove('visible');
+        sci.classList.remove('visible');
+        img1.classList.remove('grayc');
+        img2.classList.remove('grayc');
+        img3.classList.remove('grayc');
+        img4.classList.remove('grayc');
+        img5.classList.remove('grayc');
+        img6.classList.remove('grayc');
+    }
+    if(elemId == 'img2'){
+        koi.classList.add('visible');
+        mai.classList.add('visible');
+        eni.classList.remove('visible');
+        hii.classList.remove('visible');
+        soi.classList.remove('visible');
+        sci.classList.remove('visible');
+        img1.classList.add('grayc');
+        img2.classList.add('grayc');
+        img3.classList.remove('grayc');
+        img4.classList.remove('grayc');
+        img5.classList.remove('grayc');
+        img6.classList.remove('grayc');
+    }
+    if(elemId == 'img3'){
+        ci.classList.add('visible');
+        ai.classList.remove('visible');
+        di.classList.add('visible');
+        pi.classList.remove('visible');
+        img1.classList.add('grayc');
+        img2.classList.remove('grayc');
+        img3.classList.add('grayc');
+        img4.classList.remove('grayc');
+    }
+    if(elemId == 'img4'){
+        ci.classList.add('visible');
+        ai.classList.remove('visible');
+        di.classList.remove('visible');
+        pi.classList.add('visible');
+        img1.classList.add('grayc');
+        img2.classList.remove('grayc');
+        img3.classList.remove('grayc');
+        img4.classList.add('grayc');
+    }
+}
