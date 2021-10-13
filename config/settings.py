90cef0999ff4667fa0cfb37c8df2593fc28fbd3d
KwakHyuk95 <rhkrgur95@gmail.com>
2021-10-13 17:03:50 +0900

    211013 3

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'suit',
     'common.apps.CommonConfig',
     'mathboard.apps.MathboardConfig',
     'krboard.apps.KrboardConfig',
--- enboard/views.py
+++ enboard/views.py
@@ -59,9 +59,8 @@ def index(request):
     # 페이징 처리 기능 구현
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩
     page_obj = paginator.get_page(page)
-    context ={'question_list': page_obj}
-    # 템플릿단에 던짐 / 변수에 담지않고 던져도 상관 X
-    return render(request, 'enboard/question_list.html', context) # render는 조회성
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
+    return render(request, 'krboard/question_list.html', context)
 
 
 def detail(request, question_id):
